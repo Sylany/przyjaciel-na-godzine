f02d0f7332df0f45aa574f963adf5642a5bac250
Andreas <83499839+Sylany@users.noreply.github.com>
2025-11-21 04:38:30 +0100

    Delete templates directory

--- templates/my-listings.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('ABSPATH')) {
-    exit;
-}
-// Szablon listy ogłoszeń użytkownika
-echo do_shortcode('[png_my_listings]');
--- templates/user-profile.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!defined('ABSPATH')) {
-    exit;
-}
-// Szablon profilu użytkownika
-echo do_shortcode('[png_user_profile]');
